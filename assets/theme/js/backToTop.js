19348c2ab6a0355d23612b9a4d632b3d1f52e228
JohnMaher14 <john@digitalbondmena.com>
2022-09-22 17:47:23 +0200

    structure updates

--- assets/theme/js/backToTop.js
+++ assets/theme/js/backToTop.js
@@ -14,17 +14,20 @@
 
 //   })
 // }(jQuery);
-$(document).ready(function(s){
+$(document).ready(function(){
         var e = document.querySelector(".progress-wrap path");
         var  t = e?.getTotalLength();
-        e.style.transition = e.style.WebkitTransition = "none", e.style.strokeDasharray = t + " " + t, e.style.strokeDashoffset = t, e.getBoundingClientRect(), e.style.transition = e.style.WebkitTransition = "stroke-dashoffset 10ms linear";
+        e.style.transition = e.style.WebkitTransition = "none",
+         e.style.strokeDasharray = t + " " + t,
+          e.style.strokeDashoffset = t, e.getBoundingClientRect(),
+           e.style.transition = e.style.WebkitTransition = "stroke-dashoffset 10ms linear";
         var o = function () {
-          var o = s(window).scrollTop();
-          var  r = s(document).height() - s(window).height()
+          var o = $(window).scrollTop();
+          var  r = $(document).height() - $(window).height()
           var  i = t - o * t / r;
           e.style.strokeDashoffset = i
         };
-        o(), s(window).scroll(o);
+        o(), $(window).scroll(o);
 
 
   $('.progress-wrap').on('click', function () {
